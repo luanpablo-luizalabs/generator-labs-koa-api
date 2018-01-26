50fb9cccefac5e206c37241d93e1ae9c12327682
Luan Fabiano <luanpab@gmail.com>
2018-01-26 15:41:02 -0200

    prettier fixes

--- __tests__/app.js
+++ __tests__/app.js
@@ -1,17 +1,15 @@
-'use strict';
 const path = require('path');
 const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 
 describe('generator-labs-koa-api:app', () => {
   beforeAll(() => {
-    return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({someAnswer: true});
+    return helpers
+      .run(path.join(__dirname, '../generators/app'))
+      .withPrompts({ someAnswer: true });
   });
 
   it('creates files', () => {
-    assert.file([
-      'dummyfile.txt'
-    ]);
+    assert.file(['dummyfile.txt']);
   });
 });
--- generators/app/index.js
+++ generators/app/index.js
@@ -1,4 +1,3 @@
-'use strict';
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
@@ -6,16 +5,20 @@ const yosay = require('yosay');
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the impeccable ' + chalk.red('generator-labs-koa-api') + ' generator!'
-    ));
+    this.log(
+      yosay(
+        'Welcome to the impeccable ' + chalk.red('generator-labs-koa-api') + ' generator!'
+      )
+    );
 
-    const prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+    const prompts = [
+      {
+        type: 'confirm',
+        name: 'someAnswer',
+        message: 'Would you like to enable this option?',
+        default: true
+      }
+    ];
 
     return this.prompt(prompts).then(props => {
       // To access props later use this.props.someAnswer;
